5162e48e7a7327d2334468227a88acddd0f40501
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-10-11 13:19:02 +0200

    setup

---
+++ types/cc-types-base/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="lua-types/5.3" />
+/// <reference types="@typescript-to-lua/language-extensions" />
---
+++ types/cc-types-globals/index.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="globals/colors.d.ts"/>
+/// <reference path="globals/colours.d.ts"/>
+/// <reference path="../cc-types-base/index.d.ts"/>
